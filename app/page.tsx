48284c08d3c91d04f92f6da4c084b0b05b7ed242
Alif <alifhossain56782@gmail.com>
2025-03-28 23:46:52 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className="p-8">
+
+      <h1 className="text-5xl font-cantata   text-center">This is event management system</h1>
+    </div>
+  );
+}
